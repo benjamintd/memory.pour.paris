f17b5dae1ffdf1250925065bf68ac5be1dd4eda8
Benjamin TD <benjamin.tdm@gmail.com>
2023-09-25 18:03:21 +0200

    some map sorta working

---
+++ scripts/preprocess.ts
@@ -0,0 +1,101 @@
+import * as path from "path";
+import { Feature, FeatureCollection, LineString, Point } from "geojson";
+import turf, { lineString } from "@turf/turf";
+import { groupBy, mapValues } from "lodash";
+
+const main = async () => {
+  // --- STREETS ---
+
+  const streets = Bun.file(
+    path.join(__dirname, "../public/data/voies.geojson")
+  );
+  const collection = (await streets.json()) as FeatureCollection<
+    LineString,
+    {
+      c_desi: string;
+      l_courtmin: string;
+      l_longmin: string;
+      l_voie: string;
+      length?: number;
+      objectid?: number | string;
+    }
+  >;
+  const featuresStreets = collection.features
+    .map((feature) => {
+      const length = turf.length(feature, { units: "kilometers" });
+      const id = +`${200}${feature.properties.objectid}`;
+      return {
+        ...feature,
+        properties: {
+          length,
+          short_name: feature.properties.l_courtmin,
+          long_name: feature.properties.l_longmin,
+          name: feature.properties.l_voie,
+          type: feature.properties.c_desi,
+          id,
+        },
+        id,
+      };
+    })
+    .filter(
+      (
+        f: unknown
+      ): f is Feature<
+        LineString,
+        {
+          length: number;
+          short_name: string;
+          long_name: string;
+          name: string;
+          type: string;
+          id: number | string;
+        }
+      > => !!f && !!(f as Feature).id
+    );
+
+  // --- STATIONS ---
+  const stations = Bun.file(
+    path.join(__dirname, "../public/data/stations.geojson")
+  );
+
+  const stationsCollection = (await stations.json()) as FeatureCollection<
+    Point,
+    { id_gares: number; nom_gares: string; res_com: string }
+  >;
+
+  const featuresStations = stationsCollection.features
+    .map((feature) => {
+      const id = +`${100}${feature.properties.id_gares}`;
+      return {
+        ...feature,
+        properties: {
+          id,
+          name: feature.properties.nom_gares,
+          type: "metro",
+          line: feature.properties.res_com,
+        },
+        id,
+      };
+    })
+    .filter((f) => f.properties.line.startsWith("METRO"));
+
+  Bun.write(
+    path.join(__dirname, "../public/data/features.json"),
+    JSON.stringify({
+      type: "FeatureCollection",
+      features: [...featuresStreets, ...featuresStations],
+      properties: {
+        totalLength: featuresStreets.reduce((acc, feature) => {
+          return acc + feature.properties!.length;
+        }, 0),
+        totalStations: featuresStations.length,
+        stationsPerLine: mapValues(
+          groupBy(featuresStations, (feature) => feature.properties!.line),
+          (stations) => stations.length
+        ),
+      },
+    })
+  );
+};
+
+main();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,331 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  KeyboardEventHandler,
+  useState,
+  useCallback,
+  useMemo,
+  useEffect,
+} from "react";
+import data from "../../public/data/features.json";
+import Fuse from "fuse.js";
+import { useLocalStorage } from "react-use";
+import { FeatureCollection, LineString, MultiLineString, Point } from "geojson";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const fc = data as FeatureCollection<
+  LineString | MultiLineString | Point,
+  {
+    type: string;
+    name: string;
+    id?: number | null;
+    long_name?: string;
+    short_name?: string;
+    line?: string;
+  }
+>;
 
 export default function Home() {
+  const [map, setMap] = useState<mapboxgl.Map | null>(null);
+  const [search, setSearch] = useState<string>("");
+
+  const idMap = useMemo(() => {
+    const map = new Map();
+    fc.features.forEach((feature) => {
+      map.set(feature.id, feature);
+    });
+    return map;
+  }, []);
+
+  const [found, setFound] = useLocalStorage<number[]>("paris-streets", []);
+
+  const fuse = useMemo(
+    () =>
+      new Fuse(fc.features, {
+        includeScore: true,
+        includeMatches: true,
+        keys: [
+          "properties.long_name",
+          "properties.short_name",
+          "properties.name",
+        ],
+        minMatchCharLength: 2,
+        threshold: 0.1,
+        distance: 10,
+        getFn: (obj, path) => {
+          const value = Fuse.config.getFn(obj, path);
+          if (Array.isArray(value)) {
+            return value.map((el) => removeAccents(el));
+          } else {
+            return removeAccents(value as string);
+          }
+        },
+      }),
+    []
+  );
+
+  const onKeyDown: KeyboardEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      if (e.key !== "Enter") return;
+      if (!search) return;
+
+      e.preventDefault();
+      const results = fuse.search(removeAccents(search));
+
+      const matches: number[] = [];
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+
+        if (
+          result.matches &&
+          result.matches.length &&
+          result.matches.some(
+            (match) =>
+              match.indices[0][0] < 2 &&
+              match.value!.length - match.indices[match.indices.length - 1][1] <
+                3
+          ) &&
+          (found || []).indexOf(+result.item.id!) === -1
+        ) {
+          matches.push(+result.item.id!);
+        }
+      }
+
+      setFound([...(found || []), ...matches]);
+
+      setSearch("");
+    },
+    [search, setSearch, fuse, found, setFound]
+  );
+
+  useEffect(() => {
+    if (!map) {
+      mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!;
+
+      const mapboxMap = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/benjamintd/clmyn184e02ty01r49vd07lm1",
+        bounds: [
+          [2.21, 48.815573],
+          [2.47, 48.91],
+        ],
+        zoom: 12,
+      });
+
+      mapboxMap.on("load", () => {
+        mapboxMap.addSource("paris", {
+          type: "geojson",
+          data: fc,
+        });
+
+        mapboxMap.addLayer({
+          id: "voies",
+          type: "line",
+          paint: {
+            "line-color": [
+              "case",
+              ["to-boolean", ["feature-state", "found"]],
+              "#1c6dca",
+              "rgb(208, 216, 225)",
+            ],
+            "line-width": ["interpolate", ["linear"], ["zoom"], 12, 1, 16, 4],
+          },
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          source: "paris",
+          filter: ["==", "$type", "LineString"],
+        });
+
+        mapboxMap.addLayer({
+          id: "voies-labels",
+          type: "symbol",
+          paint: {
+            "text-color": [
+              "case",
+              ["to-boolean", ["feature-state", "found"]],
+              "rgb(103, 114, 126)",
+              "rgba(0, 0, 0, 0)",
+            ],
+          },
+          layout: {
+            "text-field": ["to-string", ["get", "short_name"]],
+            "symbol-placement": "line",
+            "symbol-avoid-edges": true,
+            "text-size": ["interpolate", ["linear"], ["zoom"], 11, 13, 22, 16],
+          },
+          source: "paris",
+          filter: ["==", "$type", "LineString"],
+        });
+
+        mapboxMap.addLayer({
+          filter: ["match", ["get", "type"], ["metro"], true, false],
+          type: "circle",
+          source: "paris",
+          id: "metro-circles",
+          paint: {
+            "circle-radius": ["interpolate", ["linear"], ["zoom"], 9, 1, 16, 4],
+
+            "circle-color": [
+              "case",
+              ["to-boolean", ["feature-state", "found"]],
+              [
+                "match",
+                ["get", "line"],
+                ["METRO 1"],
+                "#ffce00",
+                ["METRO 2"],
+                "#0064b0",
+                ["METRO 3"],
+                "#9f9825",
+                ["METRO 3bis"],
+                "#98d4e2",
+                ["METRO 4"],
+                "#c04191",
+                ["METRO 5"],
+                "#f28e42",
+                ["METRO 6"],
+                "#83c491",
+                ["METRO 7"],
+                "#f3a4ba",
+                ["METRO 7bis"],
+                "#83c491",
+                ["METRO 8"],
+                "#ceadd2",
+                ["METRO 9"],
+                "#d5c900",
+                ["METRO 10"],
+                "#e3b32a",
+                ["METRO 11"],
+                "#8d5e2a",
+                ["METRO 12"],
+                "#00814f",
+                ["METRO 13"],
+                "#98d4e2",
+                ["METRO 14"],
+                "#662483",
+                "rgba(255, 255, 255, 0.8)",
+              ],
+              "rgba(255, 255, 255, 0.8)",
+            ],
+            "circle-stroke-color": [
+              "case",
+              ["to-boolean", ["feature-state", "found"]],
+              [
+                "match",
+                ["get", "line"],
+                ["METRO 1"],
+                "#806700",
+                ["METRO 2"],
+                "#003258",
+                ["METRO 3"],
+                "#4f4c12",
+                ["METRO 3bis"],
+                "#2a7f93",
+                ["METRO 4"],
+                "#602049",
+                ["METRO 5"],
+                "#90440a",
+                ["METRO 6"],
+                "#356f41",
+                ["METRO 7"],
+                "#b41843",
+                ["METRO 7bis"],
+                "#356f41",
+                ["METRO 8"],
+                "#76447c",
+                ["METRO 9"],
+                "#6a6400",
+                ["METRO 10"],
+                "#775c10",
+                ["METRO 11"],
+                "#462f15",
+                ["METRO 12"],
+                "#004027",
+                ["METRO 13"],
+                "#2a7f93",
+                ["METRO 14"],
+                "#331241",
+                "rgba(255, 255, 255, 0.8)",
+              ],
+              "rgba(255, 255, 255, 0.8)",
+            ],
+            "circle-stroke-width": [
+              "case",
+              ["to-boolean", ["feature-state", "found"]],
+              1,
+              0,
+            ],
+          },
+        });
+
+        setMap(mapboxMap);
+      });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    if (!map || !found) return;
+
+    map.removeFeatureState({ source: "paris" });
+
+    for (let id of found) {
+      map.setFeatureState({ source: "paris", id }, { found: true });
+    }
+  }, [found, map]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-row items-center justify-between h-screen">
+      <div className="relative flex justify-center h-full grow">
+        <div className="absolute top-0 left-0 w-full h-full" id="map" />
+
+        <input
+          value={search}
+          // @ts-ignore
+          onChange={(e) => setSearch((e.target as HTMLInputElement).value)}
+          id="input"
+          type="text"
+          autoFocus
+          className="absolute w-64 h-12 px-4 py-2 top-32"
+          onKeyDown={onKeyDown}
+        ></input>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="h-full p-6 overflow-y-auto w-96">
+        <button
+          onClick={() => {
+            setFound([]);
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          reset
+        </button>
+        <ol>
+          {(found || []).map((id) => {
+            const feature = idMap.get(id);
+            if (!feature) return null;
+            return (
+              <li key={id}>
+                {feature.properties.line
+                  ? `${feature.properties.line.toLowerCase()} - `
+                  : ""}
+                {feature.properties.long_name || feature.properties.name}
+              </li>
+            );
+          })}
+        </ol>
       </div>
     </main>
-  )
+  );
 }
+function lower(name: string): string {
+  return name.toLowerCase();
+}
+
+export const removeAccents = (str?: string) =>
+  (str || "")
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036F]/g, "")
+    .replace(/st /g, "saint ")
+    .replace(/st-/g, "saint-");
