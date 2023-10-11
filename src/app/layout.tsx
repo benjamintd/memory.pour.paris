657fa6fecc6e51bcd12840549ca2c3c90ceb2413
Benjamin TD <benjamin.tdm@gmail.com>
2023-10-11 16:58:09 +0200

    ad script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,11 @@ export default function RootLayout({
     <html className={font.className} lang="fr">
       <body className={font.className}>{children}</body>
       <Analytics />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7420123397062174"
+        crossOrigin="anonymous"
+      ></script>
     </html>
   );
 }
